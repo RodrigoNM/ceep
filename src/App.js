ee83b8e09d7c03970f79b76d51f10466f7e531fb
rodrigonm <rdrg.n.magalhaes@gmail.com>
2021-01-20 18:34:05 -0200

    added css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import NotesList from "./components/notes_list"
-import NotesForm from "./components/notes_form"
-import { Component } from "react";
-
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
 class App extends Component {
-  render(){
+
+  criarNota(titulo, texto){
+    console.log(`uma nova nota foi criada ` + titulo + " " + texto)
+  }
+  render() {
     return (
-      <section>
-        <NotesForm/>
-        <NotesList/>
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota}/>
+        <ListaDeNotas />
       </section>
     );
   }
 }
+
 
 export default App;
